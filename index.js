457e55c52c6423af3af5881b2b6cdcf5f66455f2
Eryk <rcksorto@yahoo.com>
2018-09-03 00:12:52 -0700

    Integrate MySQL

---
+++ index.js
@@ -0,0 +1,64 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'sacramental_registry',
+  user: '',
+  password: ''
+});
+
+function populateTable(results) {
+  const tableBody = document.getElementById('people');
+
+  for (const result of results) {
+    const values = Object.values(result).map(function (value) {
+      return '<td>' + (value ? value : '-') + '</td>';
+    });
+
+    values.shift();
+
+    const row = document.createElement('tr');
+    row.classList.add('person');
+    row.id = result.id;
+    row.innerHTML = values.join('');
+
+    tableBody.appendChild(row);
+  }
+}
+
+connection.connect(function (error) {
+  if (error) {
+    throw error;
+  }
+
+  const columns = [
+    'id',
+    'first_name',
+    'last_name',
+    'sex',
+    'DATE_FORMAT(birthdate, "%M %e, %Y") AS birthdate',
+    'birth_city',
+    'father',
+    'mother',
+    'CONCAT(home_address_line_1, IF(home_address_line_2 IS NULL, "", home_address_line_2)) AS "home_address"',
+    'city',
+    'region',
+    'zip_code',
+    'DATE_FORMAT(baptism, "%M %e, %Y") AS baptism',
+    'DATE_FORMAT(communion, "%M %e, %Y") AS communion',
+    'DATE_FORMAT(confirmation, "%M %e, %Y") AS confirmation',
+    'DATE_FORMAT(wedding, "%M %e, %Y") AS wedding',
+    'DATE_FORMAT(profession_of_faith, "%M %e, %Y") AS profession_of_faith',
+    'DATE_FORMAT(death, "%M %e, %Y") AS death',
+  ];
+
+  const query = 'SELECT ' + columns.join(', ') + ' FROM registry ORDER BY first_name LIMIT 100';
+
+  connection.query(query, function (error, results) {
+    if (error) {
+      throw error;
+    }
+
+    populateTable(results);
+  });
+});
