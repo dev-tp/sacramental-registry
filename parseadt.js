9217b3be7bf4b3cafc0e12850b69207842699aae
Eryk <rcksorto@yahoo.com>
2018-09-30 21:10:54 -0700

    Create CSV file from parseadt.js

--- parseadt.js
+++ parseadt.js
@@ -1,65 +1,73 @@
 const adt = require('node_adt');
 const fs = require('fs');
 
-const parser = new adt();
+const types = {
+  1: 'LOGICAL',
+  // 4: 'CHARACTER',
+  4: 'VARCHAR',
+  5: 'TEXT',
+  20: 'CICHARACTER',
+  26: 'NCHAR',
+  10: 'DOUBLE',
+  // 11: 'INTEGER',
+  11: 'INT',
+  15: 'AUTOINCREMENT',
+  12: 'SHORT',
+  3: 'DATE',
+  13: 'TIME',
+  14: 'TIMESTAMP',
+};
 
-parser.open('cathedral.adt', 'ISO-8859-1', function (error, table) {
-  if (error) {
-    return console.log(error);
-  }
+function adtToCsv(filename) {
+  const parser = new adt();
 
-  // console.log(table.columns);
-  console.log(table.header.recordCount);
-
-  const logger = fs.createWriteStream('entries.txt', { flags: 'a' });
-
-  table.eachRecord(function (error, record) {
+  parser.open(`${filename}.ADT`, 'ISO-8859-1', function (error, table) {
     if (error) {
-      return console.log(error);
+      throw error;
     }
 
-    const entry = {
-      'first_name': record.FIRSTNAME,
-      'last_name': record.SURNAME,
-      'male': record.MALE,
-      'female': record.FEMALE,
-      'birthdate': record.BIRTHDATE,
-      'birth_city': record.BIRTHCITY,
-      'father': record.FATHER,
-      'mother': record.MOTHER,
-      'home_address_line_1': record.ADDRESS1,
-      'home_address_line_2': record.ADDRESS2,
-      'city': record.CITY,
-      'state': record.STATE,
-      'zip_code': record.ZIP,
-      'baptism': record.BAPTDATE,
-      'baptism_chrch': record.BAPTCHRCH,
-      'communion': record.COMMDATE,
-      'confirmation': record.CONFDATE,
-      'wedding': record.WEDDATE,
-      'profession_of_faith': record.PROFDATE,
-      'death': record.BURYDATE,
-      'baptism_key': record.BAPTKEY,
-      'communion_key': record.COMMKEY,
-      'confirmation_key': record.CONFKEY,
-      'wedding_key': record.WEDDINGKEY,
-      'weddng_2_key': record.WEDDNG2KEY,
-      'profession_of_faith_key': record.PROFKEY,
-      'death_key': record.DEATHKEY,
-      'comments': record.REMARKS,
-      // 'acceptance': record.ACCEPTANCE,
-      // 'accptkey': record.ACCPTKEY,
-      // 'bapdkey': record.BAPDKEY,
-      // 'baptism': record.BAPTISM,
-      // 'communion': record.COMMUNION,
-      // 'confirmation': record.CONFIRMATI,
-      // 'death': record.DEATH,
-      // 'marriage': record.MARRIAGE,
-      // 'marriage2': record.MARRIAGE2,
-      // 'parish_code': record.PARISHCODE,
-      // 'profofaith': record.PROFOFAITH,
-    };
+    console.log(`${filename} Columns:`);
 
-    logger.write(JSON.stringify(entry) + '\n');
+    table.columns.forEach(function (column) {
+      if (column.type != 4) {
+        console.log(column.name.toLowerCase() + ' ' + types[column.type]);
+      } else {
+        const length = column.length % 2 == 1 ? column.length + 1 : column.length;
+        console.log(column.name.toLowerCase() + ' ' + types[column.type] + ' ' + length);
+      }
+    });
+
+    const columnNames = table.columns.map(column => column.name);
+
+    fs.writeFileSync(`${filename}.csv`, columnNames.join(', ') + '\n');
+
+    table.eachRecord(function (error, record) {
+      if (error) {
+        throw error;
+      }
+
+      const values = columnNames.map(function (columnName) {
+        if (columnName.toLowerCase().includes('date') && record[columnName] != null) {
+          const date = [
+            record[columnName].getFullYear(),
+            record[columnName].getMonth() + 1,
+            record[columnName].getDate(),
+          ];
+
+          date[1] = date[1] < 10 ? '0' + date[1] : date[1];
+          date[2] = date[2] < 10 ? '0' + date[2] : date[2];
+
+          record[columnName] = date.join('-');
+        }
+
+        return JSON.stringify(record[columnName]);
+      }).join(', ');
+
+      fs.appendFileSync(`${filename}.csv`, `${values}\n`);
+    });
   });
-});
+}
+
+if (process.argv.length > 2) {
+  adtToCsv(process.argv[2].split('.')[0]);
+}
