330b73da3fd4cddc00bc4bb465abcc2e32d07e72
Eryk <rcksorto@yahoo.com>
2019-08-20 16:28:01 -0700

    Separate form tabs into individual components

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,6 @@
 document.getElementById('form__cancel-button').onclick = function () {
   switchSection('search-form');
 };
+
+loadComponent('components/form/profile.html', 'form__inputs');
+loadComponent('components/form/baptism.html', 'form__inputs');
