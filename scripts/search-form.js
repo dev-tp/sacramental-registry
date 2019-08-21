b9bd9f645728baffb563c48dfa309a8e49d45b5d
Eryk <rcksorto@yahoo.com>
2019-08-21 15:28:01 -0700

    Setup an input to search by date

--- scripts/search-form.js
+++ scripts/search-form.js
@@ -1,3 +1,16 @@
 document.getElementById('search-form__create-button').onclick = function () {
   switchSection('form');
 };
+
+document.getElementById('search-form__date-search-button').onclick = function () {
+  $('#search-form__normal-search').toggle();
+  $('#search-form__date-search').toggle();
+
+  this.checked = !this.checked;
+
+  if (this.checked) {
+    this.querySelector('i').classList.add('text-primary');
+  } else {
+    this.querySelector('i').classList.remove('text-primary');
+  }
+}
