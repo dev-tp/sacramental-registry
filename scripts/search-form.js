aef69ab7778308b7ce9a2e5a59601881436e5e68
Eryk <rcksorto@yahoo.com>
2019-08-21 16:58:09 -0700

    Display search results

--- scripts/search-form.js
+++ scripts/search-form.js
@@ -1,3 +1,34 @@
+function displayResults(results) {
+  const containerElem = document.querySelector('#search-form__results');
+
+  containerElem.innerHTML = '';
+
+  for (const result of results) {
+    const name = `${result['first_name']} ${result['last_name']}`;
+    const address = [
+      result['home_address_line_1'],
+      result['home_address_line_2'] ? result['home_address_line_2'] : '',
+      result['city'],
+      result['region'],
+      result['zip_code']
+    ].join(' ');
+
+    const resultElem = document.createElement('div');
+    resultElem.id = result['id'];
+    resultElem.className = 'search-form__result';
+    resultElem.innerHTML =
+      `<p class="search-form__result-name">${name}</p>` +
+      `<p class="search-form__result-address">${address}</p>` +
+      `<p class="search-form__result-mother">Mother: ${result['mother'] ? result['mother'] : '&ndash;'}</p>` +
+      `<p class="search-form__result-father">Father: ${result['father'] ? result['father'] : '&ndash;'}</p>`;
+    resultElem.onclick = function () {
+      // TODO Display result in form
+    };
+
+    containerElem.appendChild(resultElem);
+  }
+}
+
 document.getElementById('search-form__create-button').onclick = function () {
   switchSection('form');
 };
@@ -14,3 +45,25 @@ document.getElementById('search-form__date-search-button').onclick = function ()
     this.querySelector('i').classList.remove('text-primary');
   }
 }
+
+document.getElementById('search-form__normal-search').onkeyup = function () {
+  if (this.timeout) {
+    clearTimeout(this.timeout);
+  }
+
+  if (this.value && this.value.length > 2) {
+    // Delay 500ms before sending a request to the database
+    this.timeout = setTimeout(() => {
+      const value = JSON.stringify('%' + this.value + '%');
+      const query = `SELECT * FROM registry WHERE CONCAT(first_name, " ", last_name) LIKE ${value} AND deleted = 0`;
+
+      database.query(query, function (error, results) {
+        if (error) {
+          throw error;
+        }
+
+        displayResults(results);
+      });
+    }, 500);
+  }
+};
