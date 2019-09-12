035f41638a2c7203e3fb353384709819ebfe01ea
Eryk <rcksorto@yahoo.com>
2019-09-12 16:25:17 -0700

    Add scrollable tabs to form

--- src/renderer/pages/Form.tsx
+++ src/renderer/pages/Form.tsx
@@ -1,6 +1,69 @@
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
 import React from 'react';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 import Typography from '@material-ui/core/Typography';
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+function TabPanel(props: TabPanelProps): JSX.Element {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
 export default function Form(): JSX.Element {
-  return <Typography>Form</Typography>;
+  const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+
+  const categories = ['Profile', 'Baptism', 'Communion', 'Confirmation', 'Marriage', 'Profession of Faith'];
+
+  function handleChange(_: React.ChangeEvent<{}>, newValue: number) {
+    setValue(newValue);
+  }
+
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Tabs value={value} onChange={handleChange} variant="scrollable">
+            {categories.map(label => <Tab label={label} />)}
+          </Tabs>
+        </AppBar>
+        {categories.map((category, i) => <TabPanel value={value} index={i}>{category}</TabPanel>)}
+      </div>
+    </>
+  );
 }
