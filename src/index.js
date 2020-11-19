c10323b40b57dbf52f5b9499e5e66f02eb8832e4
Eryk <rcksorto@yahoo.com>
2020-11-19 10:31:35 -0800

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import reducers from './reducers';
 
 import './index.css';
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
