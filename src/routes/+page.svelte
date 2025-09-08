eeb78c11cd5087dc5c8eb410bee234f6f73d0d46
Erik Sorto <rcksorto@yahoo.com>
2025-09-08 00:12:46 -0700

    Don't wrap text on rows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,7 +57,7 @@
 		<div class="overflow-auto">
 			<table class="min-w-full">
 				<thead>
-					<tr class="border-b border-gray-300 *:p-2">
+					<tr class="border-b border-gray-300 *:p-2 *:whitespace-nowrap">
 						<th class="sticky left-0"><input class="h-5 w-5" type="checkbox" /></th>
 						<th class="hover:bg-gray-100">First Name</th>
 						<th class="hover:bg-gray-100">Middle Name</th>
@@ -74,13 +74,13 @@
 				<tbody>
 					{#each data.records as record}
 						<tr
-							class="border-b border-gray-300 *:cursor-pointer *:p-2 last:border-none hover:bg-gray-100"
+							class="border-b border-gray-300 *:cursor-pointer *:p-2 *:whitespace-nowrap last:border-none hover:bg-gray-100"
 							onclick={() => {
 								selected = record;
 								modal.show();
 							}}
 						>
-							<th class="sticky left-0">
+							<th class="sticky left-0 flex items-center justify-center">
 								<input
 									class="h-5 w-5"
 									onclick={(event) => event.stopPropagation()}
